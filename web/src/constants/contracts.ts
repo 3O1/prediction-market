1f24811b9e049541903b33dbeb0eea952f4dbaf1
3O1 <james@meetz.so>
2024-12-21 11:15:50 -0500

    Project setup

---
+++ web/src/constants/contracts.ts
@@ -0,0 +1,20 @@
+import { client } from '@/app/client';
+import { getContract } from 'thirdweb';
+import { polygonAmoy } from 'thirdweb/chains';
+
+export const predictionMarketContractAddress =
+  '0xD03E0AbA3f568942184C4Ec22696453d1a29Db89';
+export const tokenContractAddress =
+  '0x16771a901A5D5a85b3B1CC49F723723cdC0B90E5';
+
+export const predictionMarketContract = getContract({
+  client: client,
+  chain: polygonAmoy,
+  address: predictionMarketContractAddress,
+});
+
+export const tokenContract = getContract({
+  client: client,
+  chain: polygonAmoy,
+  address: tokenContractAddress,
+});
